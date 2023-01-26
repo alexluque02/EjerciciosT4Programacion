c01c3f0f54aa4861a5416ee636f43e6295a1be7d
Alexander Luque Hoffrogge <alexanderluquehoffrogge@gmail.com>
2023-01-26 14:06:38 +0100

    Corrección ej 3 principalç

--- EjerciciosT4/src/ejercicio3/Principal.java
+++ EjerciciosT4/src/ejercicio3/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		int menu, cantidadFija, potencia, cilindrada;
-		Vehiculo v=new Vehiculo(200, 120, "tipo b", 200);
+		Vehiculo v=new Vehiculo(200, 120, "tipo b", 0);
 		Motocicleta m;
 		Coche c;
 		Furgoneta f;
@@ -28,19 +28,19 @@ public class Principal {
 			case 2:
 				System.out.println("Diga la cilindrada de la moto");
 				cilindrada=Leer.datoInt();
-				m=new Motocicleta(cilindrada, 30, "tipo a", 120);
+				m=new Motocicleta(cilindrada, 30, "tipo a", 0);
 				System.out.println(m.calcularImpuesto());
 				break;
 			case 3:
 				System.out.println("Diga la potencia del coche en cv");
 				potencia=Leer.datoInt();
-				c=new Coche(170, potencia, "tipo b", 80);
+				c=new Coche(170, potencia, "tipo b", 0);
 				System.out.println(c.calcularImpuesto());
 				break;
 			case 4:
 				System.out.println("Diga el impuesto fijo");
 				cantidadFija=Leer.datoInt();
-				f=new Furgoneta(200, 120, "ECO", 230, cantidadFija);
+				f=new Furgoneta(200, 120, "ECO", 0, cantidadFija);
 				System.out.println(f.calcularImpuesto());
 				break;
 			case 0:
