ff4f34068901d2cf9fe9e6901ece9223e0c27313
Alexander Luque Hoffrogge <alexanderluquehoffrogge@gmail.com>
2023-02-06 11:32:56 +0100

    Ej 5 casi

--- EjerciciosT4/src/ejercicio5/Oficina.java
+++ EjerciciosT4/src/ejercicio5/Oficina.java
@@ -1,6 +1,59 @@
 package ejercicio5;
 
+import java.util.Arrays;
+
 public class Oficina {
 
+	private Empleado [] lista;
+
+	public Oficina(Empleado[] lista) {
+		super();
+		this.lista = lista;
+	}
+
+	public Empleado[] getLista() {
+		return lista;
+	}
+
+	public void setLista(Empleado[] lista) {
+		this.lista = lista;
+	}
+
+	@Override
+	public String toString() {
+		return "Oficina [lista=" + Arrays.toString(lista) + "]";
+	}
 	
+	public double calcularSueldoUnEmpleado(Empleado e) {
+		return e.calcularSueldo();
+	}
+	
+	public double calcularGastado() {
+		double suma=0;
+		for (int i = 0; i < lista.length; i++) {
+			suma+=calcularSueldoUnEmpleado(lista[i]);
+		}
+		return suma;
+	}
+	
+	public Empleado findEmpleado(int nEmpleado) {
+		int i=0;
+		boolean encontrado=false;
+		
+		while(i<lista.length && !encontrado && lista[i]!=null) {
+			if(lista[i].getnEmpleado()==nEmpleado) {
+				encontrado=true;
+			}else {
+				i++;
+			}
+		}
+		
+		if(encontrado) {
+			return lista[i];
+		}else {
+			return null;
+		}
+	}
+	
+	public void 
 }
